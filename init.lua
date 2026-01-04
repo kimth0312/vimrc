557913be53a25a064913fd0418b5c07df611dfac
Kim Taehyun <kimth0312@gmail.com>
2026-01-04 12:46:16 +0900

    transfer to init.lua

--- init.lua
+++ init.lua
@@ -1,27 +1,26 @@
--- ~/.config/nvim/init.lua
-
 -----------------------------------------------------------
--- 1. 전역 설정 (Leader Key)
+-- 1. 전역 설정 (Leader Key & 전역 옵션)
 -----------------------------------------------------------
 vim.g.mapleader = " "
 local opt = vim.opt
 
 -----------------------------------------------------------
--- 2. lazy.nvim 플러그인 매니저 설치
+-- 2. lazy.nvim 설치
 -----------------------------------------------------------
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath,
-  })
+    vim.fn.system({
+        "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -----------------------------------------------------------
--- 3. 플러그인 설정 및 로드
+-- 3. 플러그인 설정 (fzf-lua 및 nvim 전용 플러그인)
 -----------------------------------------------------------
 require("lazy").setup({
-    -- [UI & 테마]
+    -- [UI & Icons]
+    { "nvim-tree/nvim-web-devicons", lazy = false },
     {
         "projekt0n/github-nvim-theme",
         lazy = false,
@@ -31,65 +30,67 @@ require("lazy").setup({
                 options = { transparent = false, styles = { comments = 'italic', keywords = 'bold' } }
             })
             vim.cmd('colorscheme github_dark_dimmed')
-            vim.cmd('highlight NERDTreeNormal guibg=#484f58')
-            vim.cmd('highlight NERDTreeEndOfBuffer guibg=#484f58')
         end
     },
-    { "vim-airline/vim-airline" },
-    { "wellle/context.vim" },
-
-    -- [탐색 및 검색]
-    { "junegunn/fzf", build = "./install --all" },
     {
-        "junegunn/fzf.vim",
-        dependencies = { "junegunn/fzf" },
-        config = function()
-            vim.g.fzf_action = {
-                ['ctrl-t'] = 'tab split',
-                ['ctrl-x'] = 'split',
-                ['ctrl-v'] = 'vsplit'
-            }
-        end
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = { options = { theme = 'auto', section_separators = '', component_separators = '' } }
     },
-    { "preservim/nerdtree" },
-    
-    -- [편집 도구]
-    { "airblade/vim-gitgutter" },
-    { "easymotion/vim-easymotion" },
-    { "sheerun/vim-polyglot" },
-    { "tpope/vim-fugitive" },
 
-    -- [터미널 토글 (ToggleTerm)]
+    -- [탐색 및 검색 (fzf-lua)]
     {
-        "akinsho/toggleterm.nvim",
-        version = "*",
+        "ibhagwan/fzf-lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
-            require("toggleterm").setup({
-                size = 15,
-                open_mapping = [[<C-\>]],
-                direction = 'horizontal',
-                shade_terminals = true,
-                start_in_insert = true,
-                persist_size = true,
-                close_on_exit = true,
+            require('fzf-lua').setup({
+                "fzf-vim",
+                keymap = { fzf = { ["ctrl-j"] = "down", ["ctrl-k"] = "up" } }
             })
-            
-            function _G.set_terminal_keymaps()
-                -- fzf 창에서는 ESC가 창을 닫도록 예외 처리
-                if vim.fn.bufname():find("fzf") then
-                    return
-                end
-                local opts = {buffer = 0}
-                vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], opts)
-                vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-                vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-                vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-                vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-                vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-            end
-            vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
         end
     },
+
+    -- [파일 트리 (nvim-tree)]
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            local function my_on_attach(bufnr)
+                local api = require('nvim-tree.api')
+                local function opts(desc)
+                    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+                end
+                api.config.mappings.default_on_attach(bufnr)
+                -- 's' 키를 눌렀을 때 VS Code 대신 nvim 내부에서 세로 분할로 열기
+                vim.keymap.set('n', 's', api.node.open.vertical, opts('Vertical Split'))
+                vim.keymap.set('n', 'i', api.node.open.horizontal, opts('Horizontal Split'))
+            end
+
+            require("nvim-tree").setup({
+                on_attach = my_on_attach,
+                sync_root_with_cwd = true,
+                update_focused_file = { enable = true, update_root = true },
+                view = { width = 31, side = "left" },
+            })
+        end
+    },
+
+    -- [편집 및 문법 (Treesitter)]
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        opts = {
+            ensure_installed = { "c", "cpp", "rust", "lua", "bash", "make" },
+            highlight = { enable = true, additional_vim_regex_highlighting = false },
+        },
+        config = function(_, opts)
+            local ok, configs = pcall(require, "nvim-treesitter.configs")
+            if ok then configs.setup(opts) end
+        end
+    },
+    { "lewis6991/gitsigns.nvim", config = true },
+    { "nvim-treesitter/nvim-treesitter-context", config = true },
+    { "phaazon/hop.nvim", branch = 'v2', config = true },
 
     -- [LSP & 자동완성]
     { "neovim/nvim-lspconfig" },
@@ -99,7 +100,7 @@ require("lazy").setup({
         "hrsh7th/nvim-cmp",
         dependencies = {
             "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path",
-            "hrsh7th/cmp-cmdline", "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip"
+            "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip"
         }
     }
 })
@@ -108,53 +109,36 @@ require("lazy").setup({
 -- 4. 기본 옵션 (임베디드 개발 최적화)
 -----------------------------------------------------------
 opt.termguicolors = true
-opt.encoding = "utf-8"
 opt.number = true
-opt.hlsearch = true
-opt.autoindent = true
-opt.cindent = true
-opt.smartindent = true
 opt.tabstop = 4
 opt.shiftwidth = 4
-opt.softtabstop = 4
-opt.expandtab = false
+opt.expandtab = false -- 커널 코딩 스타일(Tab) 준수
+opt.smartindent = true
+opt.updatetime = 250
 opt.scrolloff = 2
-opt.wildmode = "longest,list"
-opt.autowrite = true
-opt.autoread = true
-opt.backspace = "eol,start,indent"
-opt.history = 256
-opt.laststatus = 2
-opt.showmatch = true
-opt.smartcase = true
-opt.incsearch = true
-opt.ruler = true
-opt.updatetime = 0
-opt.statusline = " %<%l:%v [%P]%=%a %h%m%r %F "
+opt.mouse = "a"
 
 if vim.env.LANG and (string.sub(vim.env.LANG, 1, 2) == "ko") then
     opt.fileencoding = "korea"
 end
 
 -----------------------------------------------------------
--- 5. 창(Window) 관리 (사이드바 보호)
+-- 5. 창 관리 및 사이드바 보호 로직
 -----------------------------------------------------------
 local function is_sidebar(win_id)
     local buf_id = vim.api.nvim_win_get_buf(win_id)
     local ft = vim.api.nvim_buf_get_option(buf_id, "filetype")
-    return ft == "nerdtree" or ft == "qf" or ft == "toggleterm"
+    return ft == "NvimTree" or ft == "qf"
 end
 
 local function move_window_left()
     local cur_win = vim.api.nvim_get_current_win()
     vim.cmd('wincmd h')
     local target_win = vim.api.nvim_get_current_win()
-    if target_win ~= cur_win then
-        if is_sidebar(target_win) then
-            vim.api.nvim_set_current_win(cur_win)
-        else
-            vim.cmd('wincmd x')
-        end
+    if target_win ~= cur_win and is_sidebar(target_win) then
+        vim.api.nvim_set_current_win(cur_win)
+    elseif target_win ~= cur_win then
+        vim.cmd('wincmd x')
     end
 end
 
@@ -162,155 +146,76 @@ local function move_window_right()
     local cur_win = vim.api.nvim_get_current_win()
     vim.cmd('wincmd l')
     local target_win = vim.api.nvim_get_current_win()
-    if target_win ~= cur_win then
-        if is_sidebar(target_win) then
-            vim.api.nvim_set_current_win(cur_win)
-        else
-            vim.api.nvim_set_current_win(cur_win)
-            vim.cmd('wincmd x')
-            vim.cmd('wincmd l')
-        end
+    if target_win ~= cur_win and is_sidebar(target_win) then
+        vim.api.nvim_set_current_win(cur_win)
     end
 end
 
 local map = vim.keymap.set
-map('n', '<C-w>H', move_window_left, { desc = "Move window left" })
-map('n', '<C-w>L', move_window_right, { desc = "Move window right" })
+map('n', '<C-w>H', move_window_left)
+map('n', '<C-w>L', move_window_right)
 map('n', '<C-h>', '<C-w>h')
 map('n', '<C-j>', '<C-w>j')
 map('n', '<C-k>', '<C-w>k')
 map('n', '<C-l>', '<C-w>l')
 
 -----------------------------------------------------------
--- 6. fzf 실행 시 NERDTree 보호 로직
+-- 6. 단축키 및 명령어 (fzf-lua & Zoom & Tag)
 -----------------------------------------------------------
-local function find_files_smart()
-    if vim.bo.filetype == 'nerdtree' then
-        vim.cmd('wincmd p')
-    end
-    vim.cmd('Files')
-end
+local function get_fzf() return require('fzf-lua') end
 
-map('n', '<leader>f', find_files_smart, { silent = true, desc = "Find Files (Smart)" })
+-- :B, :R, :L 명령어
+vim.api.nvim_create_user_command('B', function() get_fzf().buffers() end, {})
+vim.api.nvim_create_user_command('R', function() get_fzf().live_grep() end, {})
+vim.api.nvim_create_user_command('L', function() get_fzf().blines() end, {})
 
-map('n', '`', ':NERDTreeToggle<CR>', { silent = true })
-map('n', '<leader>Q', ':qa!<CR>', { desc = "Force Quit All" })
-map({'n', 'v'}, 'y', '"+y')
-map('n', '<C-w>]', ':vsp<CR><C-w>l:execute "tag " . expand("<cword>")<CR>')
+map('n', '<leader>f', function()
+    if vim.bo.filetype == 'NvimTree' then vim.cmd('wincmd p') end
+    get_fzf().files()
+end)
 
------------------------------------------------------------
--- 7. Zoom Toggle (NERDTree & Terminal 크기 고정)
------------------------------------------------------------
-local function restore_fixed_windows()
-    local cur_win = vim.api.nvim_get_current_win()
-    local wins = vim.api.nvim_tabpage_list_wins(0)
-    
-    for _, win in ipairs(wins) do
-        if win ~= cur_win then
-            local buf = vim.api.nvim_win_get_buf(win)
-            local ft = vim.bo[buf].filetype
-            
-            if ft == 'nerdtree' then
-                vim.api.nvim_win_set_width(win, 31)
-            elseif ft == 'toggleterm' then
-                vim.api.nvim_win_set_height(win, 15)
-            end
-        end
-    end
-end
+-- [수정 2] Ctrl+w+] 를 세로 분할 태그 점프로 변경
+map('n', '<C-w>]', ':vsp<CR><C-w>l:execute "tag " . expand("<cword>")<CR>', { desc = "Vertical split tag jump" })
 
+-- [수정 3] Zoom Toggle 시 NvimTree 고정 로직
 map('n', '<leader>z', function()
     if vim.t.zoomed then
         vim.cmd('wincmd =')
-        local wins = vim.api.nvim_tabpage_list_wins(0)
-        for _, win in ipairs(wins) do
-            local ft = vim.bo[vim.api.nvim_win_get_buf(win)].filetype
-            if ft == 'nerdtree' then vim.api.nvim_win_set_width(win, 31)
-            elseif ft == 'toggleterm' then vim.api.nvim_win_set_height(win, 15) end
+        -- 복구 시 NvimTree 너비 재설정
+        for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+            if vim.bo[vim.api.nvim_win_get_buf(win)].filetype == 'NvimTree' then
+                vim.api.nvim_win_set_width(win, 31)
+            end
         end
         vim.t.zoomed = false
-        print("Windows Restored")
     else
         vim.cmd('wincmd _')
         vim.cmd('wincmd |')
-        restore_fixed_windows()
-        vim.t.zoomed = true
-        print("Window Maximized")
-    end
-end, { desc = "Toggle Zoom" })
-
------------------------------------------------------------
--- 8. NERDTree 자동 동기화 (Sync) ★ 복구된 부분
------------------------------------------------------------
-vim.api.nvim_create_autocmd("BufEnter", {
-    group = vim.api.nvim_create_augroup("NERDTreeSync", { clear = true }),
-    callback = function()
-        -- 실제 파일이거나 일반 버퍼일 때만 동작
-        if vim.bo.buftype ~= "" or vim.bo.filetype == "nerdtree" or vim.fn.expand("%") == "" then
-            return
-        end
-        
-        -- 현재 탭에 NERDTree가 열려 있는지 확인
-        local nt_win = nil
+        -- 최대화 후에도 NvimTree 너비 고정 (핵심 해결책)
         for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
-            if vim.bo[vim.api.nvim_win_get_buf(win)].filetype == "nerdtree" then
-                nt_win = win
-                break
+            if vim.bo[vim.api.nvim_win_get_buf(win)].filetype == 'NvimTree' then
+                vim.api.nvim_win_set_width(win, 31)
             end
         end
-        
-        -- NERDTree가 열려 있다면 현재 파일 위치를 찾아서 표시
-        if nt_win then
-            vim.cmd("silent! NERDTreeFind")
-            vim.cmd("wincmd p") -- 포커스를 다시 파일 편집창으로
-        end
-    end,
-})
+        vim.t.zoomed = true
+    end
+end, { desc = "Toggle Zoom with fixed Tree" })
 
------------------------------------------------------------
--- 9. 기타 단축키 및 명령어
------------------------------------------------------------
 map('n', '<F1>', ':tabprevious<CR>')
 map('n', '<F2>', ':tabnew<CR>')
 map('n', '<F3>', ':tabnext<CR>')
-map('n', 'gl', vim.diagnostic.open_float, { desc = "Show diagnostic" })
-map('n', '[d', vim.diagnostic.goto_prev)
-map('n', ']d', vim.diagnostic.goto_next)
-
-vim.api.nvim_create_autocmd("BufReadPost", {
-    callback = function()
-        local mark = vim.api.nvim_buf_get_mark(0, '"')
-        if mark[1] > 0 and mark[1] <= vim.api.nvim_buf_line_count(0) then
-            pcall(vim.api.nvim_win_set_cursor, 0, mark)
-        end
-    end,
-})
-
-if vim.fn.has("cscope") == 1 then
-    opt.cscopetag = true
-    opt.cscopeverbose = true
-    if vim.fn.filereadable("cscope.out") == 1 then vim.cmd("silent! cs add cscope.out") end
-end
-
-vim.api.nvim_create_user_command('B', 'Buffers', {})
-vim.api.nvim_create_user_command('L', 'Lines', {})
-vim.api.nvim_create_user_command('R', 'Rg', {})
-
--- ★ [수정됨] 탭 순서 옮기기 (Space + , / Space + .)
--- Space + , (쉼표) : 탭을 왼쪽으로 한 칸 이동
-map('n', '<leader>,', ':-tabmove<CR>', { desc = "Move tab left" })
-
--- Space + . (마침표) : 탭을 오른쪽으로 한 칸 이동
-map('n', '<leader>.', ':+tabmove<CR>', { desc = "Move tab right" })
+map('n', '<leader>,', ':-tabmove<CR>')
+map('n', '<leader>.', ':+tabmove<CR>')
+map('n', '`', ':NvimTreeToggle<CR>', { silent = true })
 
 -----------------------------------------------------------
--- 10. LSP & 자동완성
+-- 7. LSP & 자동완성 설정
 -----------------------------------------------------------
 local lspconfig = require('lspconfig')
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require("mason-lspconfig").setup({
-    ensure_installed = { "clangd" },
+    ensure_installed = { "clangd", "rust_analyzer" },
     handlers = {
         function(server_name)
             local opts = { capabilities = capabilities }
@@ -323,25 +228,43 @@ require("mason-lspconfig").setup({
 })
 
 local cmp = require('cmp')
-local luasnip = require('luasnip')
-
 cmp.setup({
-    snippet = { expand = function(args) luasnip.lsp_expand(args.body) end },
+    snippet = { expand = function(args) require('luasnip').lsp_expand(args.body) end },
     mapping = cmp.mapping.preset.insert({
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
         ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then luasnip.expand_or_jump()
             else fallback() end
         end, { 'i', 's' }),
     }),
-    sources = cmp.config.sources({ { name = 'nvim_lsp' }, { name = 'luasnip' } }, { { name = 'buffer' }, { name = 'path' } })
+    sources = cmp.config.sources({ { name = 'nvim_lsp' } }, { { name = 'buffer' }, { name = 'path' } })
 })
 
-vim.diagnostic.config({
-    virtual_text = false,
-    signs = true,
-    underline = true,
-    update_in_insert = false,
-})
+-----------------------------------------------------------
+-- 8. Floating Terminal (투명도 복원)
+-----------------------------------------------------------
+local float_term = { buf = nil, win = nil }
+function ToggleFloatingTerminal()
+    if float_term.win and vim.api.nvim_win_is_valid(float_term.win) then
+        vim.api.nvim_win_hide(float_term.win)
+        float_term.win = nil
+        return
+    end
+    if not float_term.buf or not vim.api.nvim_buf_is_valid(float_term.buf) then
+        float_term.buf = vim.api.nvim_create_buf(false, true)
+        vim.api.nvim_buf_call(float_term.buf, function() vim.fn.termopen(vim.o.shell) end)
+    end
+    local w, h = math.floor(vim.o.columns * 0.8), math.floor(vim.o.lines * 0.8)
+    float_term.win = vim.api.nvim_open_win(float_term.buf, true, {
+        relative = "editor", width = w, height = h,
+        col = (vim.o.columns - w) / 2, row = (vim.o.lines - h) / 2,
+        style = "minimal", border = "rounded",
+    })
+
+    -- [수정 1] 투명도(winblend) 설정 복원
+    vim.api.nvim_set_option_value("winblend", 20, { scope = "local", win = float_term.win })
+
+    vim.cmd("startinsert")
+end
+map({ "n", "t" }, [[<C-\>]], ToggleFloatingTerminal)
