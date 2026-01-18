46529a6071d5160b7e1ed5ea7316fd72ee1d7144
Kim Taehyun <kimth0312@gmail.com>
2026-01-18 10:02:14 +0900

    git diff in neovim

--- init.lua
+++ init.lua
@@ -132,6 +132,36 @@ require("lazy").setup({
 		-- <author_time>: 시간
 		-- <summary>: 커밋 메시지
 		current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary> <abbrev_sha>',
+
+		-- 2. 단축키 설정 (on_attach 내부)
+		on_attach = function(bufnr)
+		  local gs = require('gitsigns') -- 여기가 수정됨 (더 안전함)
+
+		  local function map(mode, l, r, opts)
+			opts = opts or {}
+			opts.buffer = bufnr
+			vim.keymap.set(mode, l, r, opts)
+		  end
+
+		  -- Navigation
+		  map('n', ']c', function()
+			if vim.wo.diff then return ']c' end
+			vim.schedule(function() gs.next_hunk() end)
+			return '<Ignore>'
+		  end, { expr = true, desc = "Next Hunk" })
+
+		  map('n', '[c', function()
+			if vim.wo.diff then return '[c' end
+			vim.schedule(function() gs.prev_hunk() end)
+			return '<Ignore>'
+		  end, { expr = true, desc = "Prev Hunk" })
+
+		  -- Actions
+		  map('n', '<leader>hp', gs.preview_hunk, { desc = "Preview Hunk" })
+		  map('n', '<leader>hd', gs.diffthis, { desc = "Diff This" })
+		  map('n', '<leader>hD', function() gs.diffthis('~') end, { desc = "Diff Last Commit" })
+		  map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = "Toggle Blame" })
+		end,
 	  }
 	},
     { "nvim-treesitter/nvim-treesitter-context", 
